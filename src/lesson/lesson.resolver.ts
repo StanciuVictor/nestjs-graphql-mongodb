2b6a0f103eb0271a3aec1f536ae83b8592be54e1
StanciuVictor <victor.stanciu@yahoo.com>
2022-04-19 16:43:40 +0300

    Add createLEsson Mutation in Resolver

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,12 +1,15 @@
 //* A resolver is the equivalent for a Controller in RESTful services. It handles incoming requests and then returns the response
 
-import { Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
 
 // Specify what this Resolver actually resolves => LessonType
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 @Resolver((of) => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
   //* Define queries or mutations. Queries - retrieve data / Mutations - create new data or change existing data
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -22,7 +25,12 @@ export class LessonResolver {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   @Mutation((returns) => LessonType)
-  createLesson() {
-    /**/
+  createLesson(
+    // Retrieve arguments to create the lesson
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
   }
 }
