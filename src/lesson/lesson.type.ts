8bf65799d62be184803b6813a297978e1763d341
StanciuVictor <victor.stanciu@yahoo.com>
2022-04-19 11:39:29 +0300

    Define the Lesson GraphQL Type

---
+++ src/lesson/lesson.type.ts
@@ -0,0 +1,22 @@
+// Type is a GraphQL concept and must be defined
+
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+
+// The type will be named LessonType by default (class name). We overwrite it by specifiyng the name in the decorator => Lesson
+@ObjectType('Lesson')
+export class LessonType {
+  //* Define class properties (fields of the Lesson)
+
+  // For ID, it's a good idea to define the type explicitly
+  @Field((type) => ID)
+  id: string;
+
+  @Field()
+  name: string;
+
+  @Field()
+  startDate: string;
+
+  @Field()
+  endDate: string;
+}
